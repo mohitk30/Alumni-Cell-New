1fc8c3b73b3103209ddf1bbb01f636c81d066b34
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-01-15 19:47:41 +0530

    Updates in events section

--- utility/events.php
+++ utility/events.php
@@ -3,31 +3,34 @@
 <div class="event_container">
     <!-- write your code below this line and also don't remove this line -->
     <div class="event-img">
-   
-  <img class="event-image"  src="./7450159.png" alt="image">
-   
-
+ <img class="event-image"  src="./7450159.png" alt="image">
+ <div class="upcom-evt">No Upcoming Event</div>
 </div>
+
+
 <div class="events">
-  <div class="heading">
-    Events
-  </div>
-  <div class="event-card-1">
-    <div class="event-type">Alumni Meet</div>
-    <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
-    <div class="event-date-1">13 Dec</div>
-  </div>
-  <div class="event-card-2">
-    <div class="event-type">Alumni Meet</div>
-    <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
-    <div class="event-date-1">10 July</div>
-  </div>
-  <div class="event-card-3">
-    <div class="event-type">Alumni Meet</div>
-    <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
-    <div class="event-date-1">22 June </div>
-  </div>
+ <div class="heading">
+   Events
+ </div>
+ <div class="event-card-1">
+   <div class="event-type">Alumni Meet</div>
+   <div class="event-date-1">13 Dec</div>
+   <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
+
+ </div>
+
+ <div class="event-card-2">
+   <div class="event-type">Alumni Meet</div>
+   <div class="event-date-1">10 July</div>
+   <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
+
+ </div>
+ <div class="event-card-3">
+   <div class="event-type">Alumni Meet</div>
+   <div class="event-date-1">22 June </div>
+   <div class="event-title-1">NIT Delhi Alumni Meet 2022</div>
+
+ </div>
 
 </div>
-<div class="upcom-evt">No Upcoming Event</div>
 </div>
