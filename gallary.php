e7b1d9402571cfc2b1d4fb51ce16f840cbdd4caa
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-01-16 08:58:52 +0530

    Gallery Section

--- gallary.php
+++ gallary.php
@@ -4,7 +4,16 @@
 <!-- write your code below this line and also don't remove this line -->
 
 <div class="gallary_container">
-    
+<div class="img-container">
+      <img class="gallery-img"src="gallery-1.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-2.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-1.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-2.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-1.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-2.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-1.png" alt="gallery-img">
+      <img class="gallery-img"src="gallery-2.png" alt="gallery-img">
+  </div>
 </div>
 
 <?php include('/utility/footer.php') ?>
