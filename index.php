0ea5084e89a7cefc5d1960f8d7a2123ba295f06c
Shreyansh Raj <211230051@nitdelhi.ac.in>
2023-01-31 21:31:03 +0530

    index page changes

--- index.php
+++ index.php
@@ -3,6 +3,117 @@
 
 <div class="home_container">
     <!-- write your code below this line and also don't remove this line -->
+    <div class="register">
+      <div class="title-h">Stay <span class="connect">Connected</span> with NIT Delhi Family</div>
+      <span class="subtitle-h">NIT Delhi Alumni Network</span>
+      <div class="register-h">
+        Register new alumni
+      </div>
+    </div>
+    <div class="home-img-div">
+      <img class="home-img" src="Charco Mobile Life.png" alt="img">
+    </div>
+    <div class="connect-h">
+      <span class="c-heading">Stay Connected with Us</span>
+      <div class="lilogo">
+        <img src="blue-linkedin-logo-15916.png" alt="img">
+        <div>nitdelhi-alumnicell</div>
+      </div>
+      <div class="flogo">
+        <img src="facebook-2870.png" alt="flogo">
+        <div>nitdelhi_alumni_cell</div>
+      </div>
+      <div class="inslogo">
+        <img src="instagram-logo-pink-10678.png" alt="inslogo">
+        <div>@nitdelhi-alumnicell</div>
+      </div>
+      <div class="twlogo">
+        <img src="twitter-circle-blue-logo-16617.png" alt="twlogo">
+        <div>nitdelhi_alumnicell</div>
+      </div>
+    </div>
+    <div class="our-alumni">
+      <div class="Alumni-h">Our Alumni</div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="Alumni-card">
+        <img class="alumni-img" src="Rectangle 8.png" alt="img">
+        <div>Mr. Yash Bhatnagar</div>
+        (Software Engineer at Google)
+        <div>
+
+
+        <img class="logo" src="li-logo.png" alt="lilogo">
+        <img class="logo" src="ins-logo.png" alt="inslogo">
+        <img class="logo" src="twit-logo.png" alt="twitlogo">
+        </div>
+      </div>
+      <div class="link">
+          View More...
+      </div>
+
+
+    </div>
+
+    
     <?php include './utility/events.php' ?>
     <?php 
        //   include './gallary.php'
