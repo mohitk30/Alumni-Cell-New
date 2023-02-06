3f527e31b587a0aa1f9f727e6def6c2e5d0aa1c2
SunnyMaurya63 <99077350+SunnyMaurya63@users.noreply.github.com>
2023-02-07 00:30:43 +0530

    map embedded

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
       <h1>Contact Alumni Cell NIT Delhi</h1>
 
         <div class="image">
-          <img  class="first" src="C:\Users\Lenovo\OneDrive\Desktop\contact\Rectangle 27.png" alt="">
+        <iframe class="first" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.7589705333885!2d77.13104541491295!3d28.816255782345756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b1923ada2e3%3A0x1169930518add2fe!2sNational%20Institute%20of%20Technology%20Delhi!5e0!3m2!1sen!2sin!4v1675709158912!5m2!1sen!2sin" width="500" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           <div class="description">
              <h3>Get in touch</h3>
           <dl>
