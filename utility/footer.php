74da1eac5aa6c5e9e19324a7cc549c4bac8ff392
RohitBeniwal <beniwalrb177@gmail.com>
2023-02-21 13:47:25 +0530

    updated new footer

--- utility/footer.php
+++ utility/footer.php
@@ -1,33 +1,139 @@
-<!-- this file contain the code for footer -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-<div class="footer-container">
-    <!-- write your code below this line and also don't remove this line -->
-    <div class="footer">
-      <div class="flex-container">
-          <div>
-             <img class="img" src="Vector.png" alt="img">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white pt-5 py-5"
+          style="background-image: url(https://images.unsplash.com/photo-1475139441338-693e7dbe20b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80); "
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-5">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold footer-heading">Quick Links</h6>
+
+            <p> <a href="https://nitdelhi.ac.in/" class="text-white">NIT DELHI HOME PAGE</a></p>
+
+            <!-- Facebook -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #3b5998"
+               href="#!"
+               role="button"
+               ><i class="fab fa-facebook-f"></i
+              ></a>
+
+            <!-- Twitter -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #55acee"
+               href="#!"
+               role="button"
+               ><i class="fab fa-twitter"></i
+              ></a>
+
+
+            <!-- Instagram -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #ac2bac"
+               href="#!"
+               role="button"
+               ><i class="fab fa-instagram"></i
+              ></a>
+
+            <!-- Linkedin -->
+            <a
+               class="btn btn-primary btn-floating m-1"
+               style="background-color: #0082ca"
+               href="#!"
+               role="button"
+               ><i class="fab fa-linkedin-in"></i
+              ></a>
+          </div> 
+
+          
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class=" mb-4 font-weight-bold footer-heading">Information Link</h6>
+            <p>
+              <a class="text-white" href="#">HOME</a>
+            </p>
+            <p>
+              <a class="text-white" href="#">WHY RECRUITE @ NITD</a>
+            </p>
+            <p>
+              <a class="text-white" href="#">FOR RECRUITERS</a>
+            </p>
+            <p>
+              <a class="text-white" href="#">TEAM</a>
+            </p>
+            <p>
+              <a class="text-white" href="#">CONTACT</a>
+            </p>
           </div>
-            <div class="txt">
-               +91-9667650530
-            </div>
-              <div>
-                <img class="image" src="2.png" alt="img1">
-              </div>
-                <div class="txt1">
-                  alumni@nitdelhi.ac.in
-                </div>
-      </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class=" mb-4 font-weight-bold footer-heading">Contact Details</h6>
+            <p><i class="fas fa-envelope mr-3"></i> tnp@nitdelhi.ac.in</p>
+            <p><i class="fas fa-envelope mr-3"></i> tnp.nitdelhi.ac.in</p>
+            <p><i class="fa-sharp fa-solid fa-location-dot"></i> Address:
+            <br>National Institute Of Technology
+            <br>Plot No. FA7,Zone P1,
+            <br>Chandra Hospital
+            <br>GT Karnal Road, Delhi-110036
+            </p>
+          </div>
+          <!-- Grid column -->
+
+<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+  <h6 class=" mb-4 font-weight-bold footer-heading">
+    Head T&P Cell
+  </h6>
+  <p>
+    Dr. Kapil Sharma
+  </p>
+  <p>tnp@nitdelhi.ac.in</p>
+  <p>Phone: 011-33861065, 183</p>
+</div>
+<!-- Grid column -->
+          
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
     </div>
-      <div class="Dfoot">
-        <span class="copyrights">All rights reserved ® NIT Delhi</span>
+    <!-- Grid container -->
+  </footer>
+  <!-- Footer -->
 
-      </div>
-
-</div>
-
-
-
-</div>
 
 
 </body>
@@ -38,3 +144,4 @@
 <!-- <script src="/script/filename.js"></script> -->
 
 </html>
+      
