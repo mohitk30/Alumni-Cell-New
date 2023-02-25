2c8de2351703b14e425215f1a187e240b9c11e20
Satwikagrawal <satwikagarwal1@gmail.com>
2023-02-25 14:43:43 +0530

    map fixed

--- contact.php
+++ contact.php
@@ -2,57 +2,52 @@
 <!-- this file contain the contact section -->
 
 
-<div class="contact_container">
-    <!-- write your code below this line and also don't remove this line -->
-    <div class="title"> <h1>Contact Alumni Cell NIT Delhi</h1></div>
-
-<<<<<<< HEAD
-        <div class="image">
-        <iframe class="first" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3495.7589705333885!2d77.13104541491295!3d28.816255782345756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b1923ada2e3%3A0x1169930518add2fe!2sNational%20Institute%20of%20Technology%20Delhi!5e0!3m2!1sen!2sin!4v1675709158912!5m2!1sen!2sin" width="500" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          <div class="description">
-=======
-        <div class="image_map">
-         <div class="map_div"> <img  class="map_nit_delhi" src="./Rectangle 27.png" alt="" height="100%" width="100%"></div>
-         
-         <div class="description">
->>>>>>> 6221bc3f2334dbdca777aebd10eef4f68f80188a
-             <h3>Get in touch</h3>
-             <dl>
-            <dt class="icon1"> ◉Address</dt>
-
-
-              <dd class="small">
-                National Institute of Technology Delhi Sector A-7, Institutional Area, Near Satyawadi Raja Harish Chandra Hospital, Narela, Delhi – 110040
-
-              </dd>
-
-              <dt class="icon2"> ◉Email</dt>
-
-
-                <dd class="Email">
-                   alumni@nitdelhi.ac.in
-
-                </dd>
-
-              </dd>
-
-              <dt class="icon3"> ◉Phone</dt>
-
-
-                <dd class="Phone">
-                   +918755604422
-
-                </dd>
-
-
-          </dl>
-
-          </div>
+<div class="container">
+            <div class="innerwrap">
+            
+                <section class="section1 clearfix">
+                    <div class="textcenter">
+                        <span class="shtext">Contact Us</span>
+                        <span class="seperator"></span>
+                        <h1>Drop Us a Mail</h1>
+                    </div>
+                </section>
+            
+                <section class="section2 clearfix">
+                    <div class="col2 column1 first">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6991.517941066787!2d77.12885673253851!3d28.816255782345603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1b1923ada2e3%3A0x1169930518add2fe!2sNational%20Institute%20of%20Technology%20Delhi!5e0!3m2!1sen!2sin!4v1677084403447!5m2!1sen!2sin" width="none" height="none" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <div class="col2 column2 last">
+                        <div class="sec2innercont">
+                            <div class="sec2addr">
+                                <p><span class="collig">Address :</span> National Institute of Technology Delhi Sector A-7, Institutional Area, Near Satyawadi Raja Harish Chandra Hospital, Narela, Delhi – 110040</p>
+                                <p><span class="collig">Phone :</span>  +918755604422</p>
+                                <p><span class="collig">Email :</span> alumni@nitdelhi.ac.in</p>
+                            </div>
+                        </div>
+                        <div class="sec2contactform">
+                            <h3 class="sec2frmtitle">Want to Know More?? Drop Us a Mail</h3>
+                            <form action="">
+                                <div class="clearfix">
+                                    <input class="col2 first" type="text" placeholder="FirstName">
+                                    <input class="col2 last" type="text" placeholder="LastName">
+                                </div>
+                                <div class="clearfix">
+                                    <input  class="col2 first" type="Email" placeholder="Email">
+                                    <input class="col2 last" type="text" placeholder="Contact Number">
+                                </div>
+                                <div class="clearfix">
+                                    <textarea name="textarea" id="" cols="30" rows="7">Your message here...</textarea>
+                                </div>
+                                <div class="clearfix"><input type="submit" value="Send"></div>
+                            </form>
+                        </div>
+    
+                    </div>
+                </section>
+            
+            </div>
         </div>
-
-
-
-</div>
 
 <?php 
 include './utility/footer.php'
