05bc7ee39c776acb76c206b4c63a466639a00808
Mohit kumar <mohitmkkumar98@gmail.com>
2023-02-27 14:58:33 +0530

    final changes

--- contact.php
+++ contact.php
@@ -3,7 +3,6 @@
 <!-- this file contain the contact section -->
 
 
-<<<<<<< HEAD
 <div class="container">
             <div class="innerwrap">
             
@@ -49,48 +48,6 @@
                 </section>
             
             </div>
-=======
-<div class="contact_container">
-    <!-- write your code below this line and also don't remove this line -->
-    <div class="title"> <h1>Contact Alumni Cell NIT Delhi</h1></div>
-
-        <div class="image_map">
-         <div class="map_div"> <img  class="map_nit_delhi" src="./Rectangle 27.png" alt="" height="100%" width="100%"></div>
-         
-         <div class="description">
-             <h3>Get in touch</h3>
-             <dl>
-            <dt class="icon1"> ◉Address</dt>
-
-
-              <dd class="small">
-                National Institute of Technology Delhi Sector A-7, Institutional Area, Near Satyawadi Raja Harish Chandra Hospital, Narela, Delhi – 110040
-
-              </dd>
-
-              <dt class="icon2"> ◉Email</dt>
-
-
-                <dd class="Email">
-                   alumni@nitdelhi.ac.in
-
-                </dd>
-
-              </dd>
-
-              <dt class="icon3"> ◉Phone</dt>
-
-
-                <dd class="Phone">
-                   +918755604422
-
-                </dd>
-
-
-          </dl>
-
-          </div>
->>>>>>> 713682a4bf9e55de725d89ad39f56e20496e230e
         </div>
 
 <?php 
